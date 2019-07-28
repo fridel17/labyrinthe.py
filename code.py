c9be9dc356debff370c1577853b22020ae6be6f0
fridel17 <45593052+fridel17@users.noreply.github.com>
2019-07-28 19:09:58 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,17 +1,45 @@
-# labyrinthe.py
 
 import pygame
 from pygame.locals import *
 
 pygame.init()
 
-fenetre = pygame.display.set_mode((4800,540))
+fenetre = pygame.display.set_mode((480,540))
 
-perso = pygame.image.load("Macgyver.PNG").convert()
+perso = pygame.image.load("Macgyver.png").convert()
 position_perso = perso.get_rect() #pour utiler les fleches du clavier
 fenetre.blit(perso,(perso, position_perso))
 
-pygame.display.flip()
+while continuer_accueil:
+	
+	#Limitation de vitesse de la boucle
+	pygame.time.Clock().tick(30)
+	
+	for event in pygame.event.get():
+		
+		#Si l'utilisateur quitte, on met les variables 
+		#de boucle à 0 pour n'en parcourir aucune et fermer
+		if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+			continuer_accueil = 0
+			continuer_jeu = 0
+			continuer = 0
+			#Variable de choix du niveau
+			choix = 0
+				
+		elif event.type == KEYDOWN:				
+			#Lancement du jeu
+			if event.key == K_F1:
+				continuer_accueil = 1	
+			
+		
+
+	#on vérifie que le joueur a bien fait un choix de niveau
+	#pour ne pas charger s'il quitte
+	if choix != 0:
+		#Chargement du fond
+		fond = pygame.image.load("tile-crusader-logo.png").convert()
+
+pygame.display.flip() #rafraichir
 
 contibuer = 1
 
@@ -21,11 +49,11 @@ while continuer:
             continuer = 0
         if event.type == KEYDOWN:
             if event.key == K_DOWN:
-                position_perso = position_perso.move(0,36)
+                position_perso = position_perso.move(0,36)#a completer
             if event.key == K_UP:
-                position_perso = position_perso.move(0,-36)
+                position_perso = position_perso.move(0,-36)# a completer
             if event.key == K_Left:
-                position_perso = position_perso.move(-32,0)
+                position_perso = position_perso.move(-32,0)# a completer
             if event.key == K_RIGHT:
-                position_perso = position_perso.move(32,O) 
+                position_perso = position_perso.move(32,O) # a completer
 
